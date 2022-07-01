53422c65bf0ad340b86292f872acaab77bddfa0c
NathanNicholson <thenathannicholson@gmail.com>
2022-07-01 12:24:40 -0500

    Change the ease of the btn animation

--- components/SignIn/index.tsx
+++ components/SignIn/index.tsx
@@ -23,15 +23,17 @@ const SignIn = () => {
       </div>
       <div className="text-center mt-5">
         <h1 className="text-5xl mb-8">Sign In</h1>
+
         {signInBtns.map(info => {
           const { id, icon, text } = info;
+
           return (
             <motion.div
               whileHover={{
                 backgroundColor: ["#E0E0E0", "#9b65ff", "#00c0fa", "#ff5329"],
               }}
               transition={{
-                ease: "easeIn",
+                ease: "easeInOut",
               }}
               className="flex items-center justify-center w-72 bg-[#E0E0E0] text-2xl py-3 rounded-md mb-4 cursor-pointer hover:text-white"
               key={id}
